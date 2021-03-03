512263447a13cb5998a03a5b879927ce289563bd
waeshang <4990590@gmail.com>
2021-03-03 10:23:00 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,4 +15,4 @@
 
 # Add a feed source
 sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
-git clone https://github.com/tohojo/sqm-scripts.git package/sqm-scripts
+#git clone https://github.com/tohojo/sqm-scripts.git package/sqm-scripts
